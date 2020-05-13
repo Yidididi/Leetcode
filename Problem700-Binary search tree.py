dc3f97b00c0c95ce3bd017bdce3638f3b423f0c8
Yidididi <49426597+Yidididi@users.noreply.github.com>
2020-05-13 18:34:24 -0400

    Add files via upload

---
+++ Problem700-Binary search tree.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 18:31:01 2020
+
+@author: MagicDi
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        def check(root,val):
+            if val == root.val:
+                return root
+            elif val < root.val:
+                if root.left:
+                    return check(root.left,val)
+                else:
+                    return
+            elif val > root.val:
+                if root.right:
+                    return check(root.right,val)
+                else:
+                    return
+        r = check(root,val)
+        return r
+
